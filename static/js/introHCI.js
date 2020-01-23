7c7bc44c0768852b1c08dcb4527177aa4d4ed683
Michele Murakami <mmurakam@ucsd.edu>
2020-01-23 10:07:38 -0800

    added some effects

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,16 +3,34 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-})
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("#testjs").click(function(e) {
+		$(".jumbotron h1").text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+		});
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	//prevent the page from reloading
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+	  	//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		description.fadeOut();
+	}
+
+
 }
